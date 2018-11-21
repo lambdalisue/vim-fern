004a82b017be18ab9d0123199288f4bd607246d9
lambdalisue <lambdalisue@hashnote.net>
2018-11-22 03:52:13 +0900

    Remove reviewdog
    https://github.com/haya14busa/reviewdog/issues/184

--- .ci/reviewdog.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-REVIEWDOG_VERSION="0.9.11"
-curl -fSL https://github.com/haya14busa/reviewdog/releases/download/$REVIEWDOG_VERSION/reviewdog_linux_amd64 -o ~/bin/reviewdog
-chmod +x ~/bin/reviewdog
