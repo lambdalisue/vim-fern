lambdalisue/vim-fern

commit 10dd4bb705fa21dbfabdafb592c7d58454af01e6

Improve hover popup behavior

autoload/fern/internal/drawer/hover_popup.vim
@@ -1,10 +1,13 @@
+let s:win = v:null
+let s:timer = 0
+
 function! fern#internal#drawer#hover_popup#init() abort
   if g:fern#disable_drawer_hover_popup
     return
   endif
 
-  if !exists('*popup_create')
-    call fern#logger#warn('hover popup is not supported, popup_create()
+  if !s:available()
+    call fern#logger#warn('hover popup is not supported, popup_create() or nvim environment
           \ does not exist. Disable this message
           \ with g:fern#disable_drawer_hover_popup.')
     return
@@ -12,52 +15,67 @@ function! fern#internal#drawer#hover_popup#init() abort
 
   augroup fern_internal_drawer_hover_popup_init
     autocmd! * <buffer>
-    autocmd CursorMoved <buffer> call s:cursor_moved_event()
+    autocmd CursorMoved <buffer> call timer_stop(s:timer) | call s:hide() | let s:timer = timer_start(g:fern#drawer_hover_popup_delay, { -> s:show(fern#helper#new()) })
+    autocmd BufLeave <buffer> call timer_stop(s:timer) | call s:hide()
   augroup END
 endfunction
 
-function! fern#internal#drawer#hover_popup#calculate_node_char_offset(node) abort
-  " find line offset where label text begins
-  let line = getline('.')
-  let labelbegin = charidx(line, strridx(line, a:node.label))
-  let labelbegin = labelbegin < 0 ? 0 : labelbegin
-
-  let windowid = win_getid()
-
-  " get cursor position in drawer window (char- and byte-indexed)
-  let charpos = getcursorcharpos(windowid)
-  let pos = getcurpos(windowid)
-
-  " get cursor position relative to screen
-  let cursorpos = screenpos(windowid, pos[1], pos[2])
-
-  " calculate screen column where label text begins
-  return cursorpos['col'] - charpos[2] + labelbegin
+function! s:available() abort
+  return has('nvim') || exists('*popup_create')
 endfunction
 
-function! fern#internal#drawer#hover_popup#should_display_popup() abort
-  return len(getline('.')) >= winwidth(0)
-endfunction
-
-function! s:cursor_moved_event() abort
-  let helper = fern#helper#new()
-
-  if fern#internal#drawer#hover_popup#should_display_popup()
-    call s:show_popup(helper)
+function! s:show(helper) abort
+  if strdisplaywidth(getline('.')) <= winwidth(0)
+    return
   endif
-endfunction
 
-function! s:show_popup(helper) abort
   let node = a:helper.sync.get_cursor_node()
   if node is# v:null
     return
   endif
 
-  let label_offset = fern#internal#drawer#hover_popup#calculate_node_char_offset(node)
-  call popup_create(l:node.label, {
-        \ 'line': 'cursor',
-        \ 'col': label_offset + 1,
-        \ 'moved': 'any',
-        \})
+  let line = getline('.')
+  if has('nvim')
+    let s:win = nvim_open_win(nvim_create_buf(v:false, v:true), v:false, {
+    \    'relative': 'win',
+    \    'bufpos': [line('.') - 2, 0],
+    \    'width': strdisplaywidth(substitute(line, '[^[:print:]]*$', '', 'g')),
+    \    'height': 1,
+    \    'noautocmd': v:true,
+    \    'style': 'minimal',
+    \  })
+  else
+    let ui_width = screenpos(0, line('.'), 1).col - win_screenpos(0)[1]
+    let s:win = popup_create(line, {
+    \    'line': 'cursor',
+    \    'col': ui_width + 1,
+    \    'maxwidth': strdisplaywidth(substitute(line, '[^[:print:]]*$', '', 'g')),
+    \  })
+  endif
+  function! s:apply() abort closure
+    call setbufline('%', 1, line)
+    setlocal nowrap
+    setlocal cursorline
+    call a:helper.fern.renderer.syntax()
+    call a:helper.fern.renderer.highlight()
+    syntax clear FernRoot
+    syntax clear FernRootText
+    if has('nvim')
+      setlocal winhighlight=NormalFloat:Normal
+    endif
+  endfunction
+  call win_execute(s:win, 'call s:apply()', v:true)
+endfunction
+
+function! s:hide() abort
+  if s:win is# v:null
+    return
+  endif
+  if has('nvim')
+    call nvim_win_hide(s:win)
+  else
+    call popup_close(s:win)
+  endif
+  let s:win = v:null
 endfunction
 

autoload/fern.vim
@@ -47,6 +47,7 @@ call s:Config.config(expand('<sfile>:p'), {
       \ 'comparators': {},
       \ 'drawer_width': 30,
       \ 'drawer_keep': v:false,
+      \ 'drawer_hover_popup_delay': 0,
       \ 'mark_symbol': '*',
       \})
 

commit 71345e74322f4d48c4378de532d557aef1dd4db6

Correct hover popup position on right side drawer
When navigating through nodes in a drawer window, node names that extend
beyond the width of the fern window are shown in a popup to display the
full name to the user.

A regression introduced in 10dd4bb (Improve hover popup behavior, 2022-03-16)
prevented the popup from being positioned correctly in drawer windows
positioned on the right side. As a result, the hover popup behavior was
disabled for right hand drawers in ab237a1 (Merge pull request #419 from
lambdalisue/fix-popup-right, 2022-04-15).

This revision reintroduces this functionality, ensuring that the popup
windows are positioned correctly in all cases.

autoload/fern/internal/drawer.vim
@@ -51,11 +51,8 @@ function! fern#internal#drawer#init() abort
   call fern#internal#drawer#auto_winfixwidth#init()
   call fern#internal#drawer#auto_restore_focus#init()
   call fern#internal#drawer#smart_quit#init()
+  call fern#internal#drawer#hover_popup#init()
   call fern#internal#drawer#resize()
-
-  if !fern#internal#drawer#is_right_drawer()
-    call fern#internal#drawer#hover_popup#init()
-  endif
 
   setlocal winfixwidth
 endfunction

autoload/fern/internal/drawer/hover_popup.vim
@@ -15,7 +15,7 @@ function! fern#internal#drawer#hover_popup#init() abort
 
   augroup fern_internal_drawer_hover_popup_init
     autocmd! * <buffer>
-    autocmd CursorMoved <buffer> call s:debounced_show()
+    autocmd CursorMoved <buffer> call s:delayed_show()
     autocmd BufLeave <buffer> call s:hide()
   augroup END
 endfunction
@@ -25,9 +25,9 @@ function! s:available() abort
   return has_win && exists('*win_execute')
 endfunction
 
-function! s:debounced_show() abort
+function! s:delayed_show() abort
   call s:hide()
-  let s:show_timer = timer_start(g:fern#drawer_hover_popup_delay, { -> s:show()  })
+  let s:show_timer = timer_start(g:fern#drawer_hover_popup_delay, { -> s:show() })
 endfunction
 
 function! s:show() abort
@@ -36,7 +36,12 @@ function! s:show() abort
   endif
   call s:hide()
 
-  if strdisplaywidth(getline('.')) <= winwidth(0)
+  " remove trailing unprintable characters
+  let line = substitute(getline('.'), '[^[:print:]]*$', '', 'g')
+  let line_width = strdisplaywidth(line)
+
+  " don't show a popup if the line fits in the window
+  if line_width < winwidth(0)
     return
   endif
 
@@ -46,23 +51,25 @@ function! s:show() abort
     return
   endif
 
-  let line = getline('.')
-  let width = strdisplaywidth(substitute(line, '[^[:print:]]*$', '', 'g'))
   if has('nvim')
     let s:win = nvim_open_win(nvim_create_buf(v:false, v:true), v:false, {
     \    'relative': 'win',
     \    'bufpos': [line('.') - 2, 0],
-    \    'width': width,
+    \    'width': line_width,
     \    'height': 1,
     \    'noautocmd': v:true,
     \    'style': 'minimal',
     \  })
   else
-    let ui_width = screenpos(0, line('.'), 1).col - win_screenpos(0)[1]
+    " calculate position of popup
+    let drawer_winid = win_getid()
+    let pos = getcurpos(drawer_winid)
+    let curpos = screenpos(drawer_winid, pos[1], 1)
+
     let s:win = popup_create(line, {
     \    'line': 'cursor',
-    \    'col': ui_width + 1,
-    \    'maxwidth': width,
+    \    'col': curpos['col'],
+    \    'maxwidth': line_width,
     \  })
   endif
 